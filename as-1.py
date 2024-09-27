3b108b6f2dc0f660071713ca937e13cd006c87a4
doykaberry <jdc9229@gmail.com>
2024-09-27 14:36:15 -0400

    Update as-1.py

--- as-1.py
+++ as-1.py
@@ -3,14 +3,14 @@
 
 completed_labs = int(input("Enter the number of labs completed: "))
 completed_quizzes = int(input("Enter the number of quizzes completed: "))
-grade_assignment1 = input("Enter grade for Assignment 1: ")
-grade_assignment2 = input("Enter grade for Assignment 2: ")
-grade_assignment3 = input("Enter grade for Assignment 3: ")
-grade_assignment4 = input("Enter grade for Assignment 4: ")
-grade_midterm1 = input("Enter grade for Midterm 1: ")
-grade_midterm2 = input("Enter grade for Midterm 2: ")
-grade_final_exam = input("Enter grade for Final Exam: ")
-grade_midterms_final_preperation = input("Enter grade for Midterms and Final Preperation: ")
+grade_assignment1 = int(input("Enter grade for Assignment 1: "))
+grade_assignment2 = int(input("Enter grade for Assignment 2: "))
+grade_assignment3 = int(input("Enter grade for Assignment 3: "))
+grade_assignment4 = int(input("Enter grade for Assignment 4: "))
+grade_midterm1 = int(input("Enter grade for Midterm 1: "))
+grade_midterm2 = int(input("Enter grade for Midterm 2: "))
+grade_final_exam = int(input("Enter grade for Final Exam: "))
+grade_midterms_final_preperation = int(input("Enter grade for Midterms and Final Preperation: "))
 
 
 #calculation of final grade and more variable assignments
@@ -22,7 +22,13 @@ completed_labs_grade = 100 if completed_labs >=6 else (completed_labs/6) * 100
 completed_quizzes_grade = 100 if completed_quizzes >=6 else (completed_quizzes/6) * 100
 
 
-    
+##variable assignments for the grade multiplied by the weight
+completed_labs_weight = completed_labs_grade * 0.20
+completed_quizzes_weight = completed_quizzes_grade * 0.15
+grade_assignments_weight = ((grade_assignment1 + grade_assignment2 + grade_assignment3 + grade_assignment4)/4) * (0.16)
+grade_midterms_weight = ((grade_midterm1 + grade_midterm2)/2) * 0.25
+grade_final_exam_weight = grade_final_exam * 0.18
+grade_midterms_final_preperation_weight = grade_midterms_final_preperation * 0.06
 
-
-
+#Final calculation
+Final_grade = completed_labs_weight + completed_quizzes_weight + grade_assignments_weight
